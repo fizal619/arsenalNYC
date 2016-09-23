a8274954ae8ec9399e8d872bc2f17092a0b34813
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-09-23 19:18:49 -0400

    trying to figure out Form

--- src/client/app/LoginUser.jsx
+++ src/client/app/LoginUser.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import NavLink from './NavLink.jsx'
+import Form from 'react-router-form'
+
 
 export default class LoginUser extends React.Component{
 
   handleSubmit(e){
     e.preventDefault();
     this.props.changeStatus();
+    this.props.router.push({
+        pathname: '/user/profile'
+      })
   }
 
   render(){
     return (
       <div>
         <section className="col-md-8 col-md-offset-2">
-          <NavLink to='/user/profile'><form action="post" onSubmit={this.handleSubmit.bind(this)}>
+          <form method="post" onSubmit={this.handleSubmit.bind(this)}>
             <div className="form-group">
               <label type='text'>User Name</label>
               <input className='form-control' type="text" name='username' placeholder="Enter User Name" required/>
@@ -22,10 +27,10 @@ export default class LoginUser extends React.Component{
               <input type="password" className='form-control' name='password_digest' placeholder="Enter Password" required/>
             </div>
             <button className='btn btn-success' type='submit'>Login</button>
-          </form></NavLink>
+          </form>
         </section>
       </div>
-      )
+    )
   }
 }
 
