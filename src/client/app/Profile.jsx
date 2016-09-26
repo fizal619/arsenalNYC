09dfd2959c7da6aa82c41e3de0184932faf08655
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-09-26 15:32:43 -0400

    firebase and dropdown issues

--- src/client/app/Profile.jsx
+++ src/client/app/Profile.jsx
@@ -7,7 +7,22 @@ export default class Profile extends React.Component {
     return (
       <div>
         <h1 className='heading'>This is the profile page!!</h1>
-        <div>
+        <div class="btn-group">
+          <div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+            <span class="caret"></span></button>
+            <ul class="dropdown-menu">
+              <li><a href="#">HTML</a></li>
+              <li><a href="#">CSS</a></li>
+              <li><a href="#">JavaScript</a></li>
+            </ul>
+          </div>
+        </div>
+        {/*<div className="dropdown">
+         <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown button
+        </button>
+          <ul className="dropdown-menu">
           {this.props.upcomingEvents.map((event, i) => {
             return (
               <li key={event.id}>{event.name}<br />
@@ -16,7 +31,8 @@ export default class Profile extends React.Component {
               {event.cost}</li>
             )
           })}
-        </div>
+          </ul>
+        </div>*/}
       </div>
       )
   }
